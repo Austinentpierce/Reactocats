3c7c5a6709d5fd4b23df20cee8c1eb9d29f128c1
Austin Pierce <Austinentpierce@gmail.com>
2021-09-09 13:45:23 -0400

    adding html into app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,53 @@
 import React from 'react'
-
+import { Reactocat } from './Components/reactocats'
 
 export function App() {
-  return <div>Hello, World</div>
+  return (
+    <body>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">
+                <img
+                  src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+                  width="45"
+                  height="45"
+                  alt="GithubLogo"
+                />
+              </a>
+            </li>
+            <li>
+              <a className="octo" href="#">
+                Octodex
+              </a>
+            </li>
+            <li>
+              <li>
+                <a href="#">Home</a>
+              </li>
+            </li>
+            <li>
+              <li>
+                <a href="https://octodex.github.com/faq/">FAQ</a>
+              </li>
+            </li>
+            <li>
+              <li>
+                <a href="#">RSS</a>
+              </li>
+            </li>
+          </ul>
+        </nav>
+        <ul>
+          <li className="twitgit">
+            <a href="https://twitter.com/elonmusk">Follow us on Twitter</a>
+            <a className="github" href="https://github.com/">
+              Back to GitHub.com
+            </a>
+          </li>
+        </ul>
+      </header>
+    </body>
+  )
 }
--- src/Components/reactocats.tsx
+++ src/Components/reactocats.tsx
@@ -1,1 +1,16 @@
 import React from 'react'
+
+type ReactocatsProps = {
+  name: string
+  number: number
+  image: string
+  creator: string
+}
+export function Reactocat(props: ReactocatsProps) {
+  return (
+    <li>
+      <a href={props.image}></a>
+      <strong>{props.name}</strong>
+    </li>
+  )
+}
